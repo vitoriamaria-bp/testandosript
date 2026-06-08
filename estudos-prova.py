844e7e6261dcf7b70324e87d930d25bfa56547d0
Vitória Maria Bezerra Pinheiro <vitoriamariabezerra72@gmail.com>
2026-06-08 18:24:13 +0000

    TwoSum

--- estudos-prova.py
+++ estudos-prova.py
@@ -1,11 +1,11 @@
-nums = [2, 7, 11, 15]
-target = 9
 
 def twoSum(nums, target):
     for i in range(len(nums)):
         for j in range(i + 1, len(nums)):
             if nums[i] + nums[j] == target:
+                print(f'indice: {i} | número = {nums[i]} + indice 2: {j} | numero 2: {nums[j]} = {target}')
                 return [i, j]
-            print(f'indice: {i} | número = {nums[i]} | indice 2: {j} | numero 2: {nums[j]}')
 
+nums = [2, 7, 11, 15]
+target = 9
 twoSum(nums, target)
